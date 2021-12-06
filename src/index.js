d1467db798103e8198ca4239eab7e28c01db543b
robthenob <92840644+robthenob@users.noreply.github.com>
2021-12-05 18:00:31 -0800

    Removed Find Location button feature

--- src/index.js
+++ src/index.js
@@ -29,10 +29,8 @@ function formatForecastDay(timestamp) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "0b44bb10c5ed254a363948e06377120f";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -60,7 +58,6 @@ function displayTemperature(response) {
   getForecast(response.data.coord);
 }
 function displayForecast(response) {
-  console.log(response.data.daily);
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
